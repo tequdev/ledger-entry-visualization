2c473348d6ff56f5a400c5cf8ce4d5e9bf9ed135
tequ <git@tequ.dev>
2024-05-07 22:34:39 +0900

    feat: Add @vercel/analytics for tracking data

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
